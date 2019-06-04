0d981c6be00f3c84f717247704fe75fe42b8ebf0
wolferhua <wolferhua@qq.com>
2019-06-04 11:25:24 +0800

    [21] 合并两个有序链表

--- 21.合并两个有序链表.py
+++ 21.合并两个有序链表.py
@@ -32,8 +32,11 @@
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
         # 确定头
-        if not (l1 and l2):
+        if not l1:
+            return l2
+        elif not l2:
             return l1
+
         head = l1
         oth = l2
         if l2.val < l1.val:
